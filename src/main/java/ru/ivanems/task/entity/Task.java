48ecba4eaf5e3a9533add75a1c2daa106cd866e8
fierysoul <ivmorozov001@gmail.com>
2024-11-18 20:11:33 +0300

    explicitly specifying the table name

--- src/main/java/ru/ivanems/task/entity/Task.java
+++ src/main/java/ru/ivanems/task/entity/Task.java
@@ -1,11 +1,9 @@
 package ru.ivanems.task.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "task")
 public class Task {
 
     @Id
