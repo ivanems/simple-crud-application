324b6373fe7b3b45e6edfe8c9dc78d9af8157975
fierysoul <ivmorozov001@gmail.com>
2024-11-27 20:16:53 +0300

    injections with lombok annotation

--- src/main/java/ru/ivanems/task/dto/TaskDTO.java
+++ src/main/java/ru/ivanems/task/dto/TaskDTO.java
@@ -1,60 +1,17 @@
 package ru.ivanems.task.dto;
 
+import lombok.*;
 import ru.ivanems.task.entity.util.TaskType;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskDTO {
 
     private Long id, userId;
     private String title, description;
     private TaskType taskType;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public TaskType getTaskType() {
-        return taskType;
-    }
-
-    public void setTaskType(TaskType taskType) {
-        this.taskType = taskType;
-    }
-
-    public TaskDTO(Long id, Long userId, String title, String description, TaskType taskType) {
-        this.id = id;
-        this.userId = userId;
-        this.title = title;
-        this.description = description;
-        this.taskType = taskType;
-    }
-
-    public TaskDTO() {}
 }
--- src/main/java/ru/ivanems/task/entity/Task.java
+++ src/main/java/ru/ivanems/task/entity/Task.java
@@ -1,9 +1,15 @@
 package ru.ivanems.task.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 import ru.ivanems.task.entity.util.TaskType;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "task")
 public class Task {
 
@@ -20,56 +26,4 @@ public class Task {
     @Enumerated(EnumType.STRING)
     private TaskType taskType;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public TaskType getTaskType() {
-        return taskType;
-    }
-
-    public void setTaskType(TaskType taskType) {
-        this.taskType = taskType;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", userId=" + userId +
-                ", taskType=" + taskType +
-                '}';
-    }
-
-    public Task() {}
 }
